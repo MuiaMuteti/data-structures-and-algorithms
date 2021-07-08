fc9fe9ea5f010e7d701a5fae5be0b04389a22164
Muia Muteti <fstsmuia@gmail.com>
2021-07-08 14:21:24 +0300

    linear search on vector of integers

--- linear-search/linear-search.cpp
+++ linear-search/linear-search.cpp
@@ -1,20 +1,29 @@
-// linear-search.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <cassert>
+
+// linear search on a vector of integers
+// uses equality as the predicate
+// return index of the location where the value is found
+// otherwise return -1
+int linearSearch(const std::vector<int>& nums, int val) {
+
+	for (int i = 0; i < nums.size(); i++) {
+		if (nums[i] == val) {
+			return i;
+		}
+	}
+
+	return -1;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::vector<int> vecNums{ 1,2,3,4,5,6,7,8,9,10 };
+
+	assert(linearSearch(vecNums, 1) == 0);
+	assert(linearSearch(vecNums, 7) == 6);
+	assert(linearSearch(vecNums, 10) == 9);
+	assert(linearSearch(vecNums, 20) == -1);
+	assert(linearSearch(vecNums, -5) == -1);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
